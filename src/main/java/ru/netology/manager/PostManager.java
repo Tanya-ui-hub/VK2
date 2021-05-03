2f62ca155cc3322c9090f9b501eb5f8abf8feb44
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-05-03 15:12:50 +0300

    added manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostInfo;
+
+public class PostManager {
+    private PostInfo[] posts;
+
+    public PostInfo[] search(int domain, int ownerId, int query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+
+}
